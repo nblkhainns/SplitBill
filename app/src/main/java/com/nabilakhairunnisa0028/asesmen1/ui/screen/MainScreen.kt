c05eb146c3f71104f91c273e2e4b164a90dacaa4
Nabila Khairunnisa <bilaaa.smiles@gmail.com>
2026-04-07 15:43:18 +0700

    Membuat navigasi ke app lain

--- app/src/main/java/com/nabilakhairunnisa0028/asesmen1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/nabilakhairunnisa0028/asesmen1/ui/screen/MainScreen.kt
@@ -1,5 +1,7 @@
 package com.nabilakhairunnisa0028.asesmen1.ui.screen
 
+import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -38,6 +40,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.ImeAction
@@ -99,6 +102,8 @@ fun ScreenContent(modifier: Modifier = Modifier){
     var totalTip by rememberSaveable { mutableDoubleStateOf(0.0)}
     var perorang by rememberSaveable { mutableDoubleStateOf(0.0) }
     var totalPerorang by rememberSaveable { mutableDoubleStateOf(0.0) }
+
+    val context = LocalContext.current
 
     Column(
         modifier = modifier.fillMaxSize()
@@ -182,6 +187,10 @@ fun ScreenContent(modifier: Modifier = Modifier){
             Text(text = stringResource(R.string.hitung))
         }
         if (totalPerorang != 0.0) {
+
+            val message = stringResource(R.string.bagikan_template,
+                total, jumlah, tip, totalTip, perorang, totalPerorang)
+
             HorizontalDivider(
                 modifier = Modifier.padding(vertical = 8.dp),
                 thickness = 1.dp
@@ -229,6 +238,13 @@ fun ScreenContent(modifier: Modifier = Modifier){
                     style = MaterialTheme.typography.titleLarge
                 )
             }
+            Button(
+                onClick = { shareData(context, message)},
+                modifier = Modifier.padding(top = 8.dp),
+                contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+            ) {
+                Text(text = stringResource(R.string.bagikan))
+            }
         }
     }
 }
@@ -271,6 +287,16 @@ private fun hitungPerorang(total: Double, jumlah: Int): Double {
 }
 private fun hitungTotalPerorang(total: Double, jumlah: Int, totalTip: Double): Double {
     return (total + totalTip)/jumlah
+}
+
+private fun shareData(context: Context, message: String){
+    val shareIntent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plai"
+        putExtra(Intent.EXTRA_TEXT, message)
+    }
+    if (shareIntent.resolveActivity(context.packageManager) != null) {
+        context.startActivity(shareIntent)
+    }
 }
 
 @Preview(showBackground = true)
