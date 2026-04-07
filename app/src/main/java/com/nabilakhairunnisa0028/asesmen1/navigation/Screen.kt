9ca596461178662b9df973d650252d37e82d8731
Nabila Khairunnisa <bilaaa.smiles@gmail.com>
2026-04-07 10:25:36 +0700

    Menambahkan Top App Bar

--- app/src/main/java/com/nabilakhairunnisa0028/asesmen1/navigation/Screen.kt
+++ app/src/main/java/com/nabilakhairunnisa0028/asesmen1/navigation/Screen.kt
@@ -1,8 +1,5 @@
 package com.nabilakhairunnisa0028.asesmen1.navigation
 
-import android.R
-import android.health.connect.datatypes.ExerciseRoute
-
 sealed class Screen(val route: String){
     data object Home: Screen("mainScreen")
 }
