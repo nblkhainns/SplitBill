1c3c35d4dfe71b16cc98814e9ef0e225d9a1e10b
Nabila Khairunnisa <bilaaa.smiles@gmail.com>
2026-04-07 11:30:10 +0700

    Meminta input data dari pengguna

--- app/src/main/java/com/nabilakhairunnisa0028/asesmen1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/nabilakhairunnisa0028/asesmen1/ui/screen/MainScreen.kt
@@ -1,17 +1,33 @@
 package com.nabilakhairunnisa0028.asesmen1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.nabilakhairunnisa0028.asesmen1.R
 import com.nabilakhairunnisa0028.asesmen1.ui.theme.Asesmen1Theme
 
@@ -37,10 +53,47 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier){
-    Text(
-        text = "Hello Android!",
-        modifier = modifier
-    )
+    var total by rememberSaveable { mutableStateOf("") }
+
+    var jumlah by rememberSaveable { mutableStateOf("") }
+
+    Column(
+        modifier = modifier.fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.app_intro),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier =modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = total,
+            onValueChange = { total = it },
+            label = {Text(text = stringResource(R.string.total_tagihan))},
+            leadingIcon = { Text(text = "Rp") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = jumlah,
+            onValueChange = { jumlah = it },
+            label = {Text(text = stringResource(R.string.jumlah_orang))},
+            trailingIcon = { Text(text = "org")},
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
 }
 
 @Preview(showBackground = true)
