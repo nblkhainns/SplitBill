92d2dad2fb799cab1d2ac8894abb082fb273e49f
Nabila Khairunnisa <bilaaa.smiles@gmail.com>
2026-04-07 15:00:08 +0700

    Membuat navigasi dengan tombol Up

--- app/src/main/java/com/nabilakhairunnisa0028/asesmen1/ui/screen/AboutScreen.kt
+++ app/src/main/java/com/nabilakhairunnisa0028/asesmen1/ui/screen/AboutScreen.kt
@@ -2,7 +2,11 @@ package com.nabilakhairunnisa0028.asesmen1.ui.screen
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -13,15 +17,26 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.nabilakhairunnisa0028.asesmen1.R
 import com.nabilakhairunnisa0028.asesmen1.ui.theme.Asesmen1Theme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AboutScreen() {
+fun AboutScreen(navController: NavController) {
     Scaffold(
         topBar = {
             TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.kembali),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                },
                 title = {
                     Text(text= stringResource(id = R.string.tentang_aplikasi))
                 },
@@ -44,7 +59,7 @@ fun AboutScreen() {
 @Composable
 fun AboutScreenPreview() {
     Asesmen1Theme() {
-        AboutScreen()
+        AboutScreen(rememberNavController())
     }
 }
 
