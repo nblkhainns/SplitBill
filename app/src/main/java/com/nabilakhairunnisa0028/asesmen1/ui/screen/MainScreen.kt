76cec822ee77ea94ecf5f9423c5c360b025ba4f1
Nabila Khairunnisa <bilaaa.smiles@gmail.com>
2026-04-07 11:53:02 +0700

    Memberi pilihan kepada pengguna

--- app/src/main/java/com/nabilakhairunnisa0028/asesmen1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/nabilakhairunnisa0028/asesmen1/ui/screen/MainScreen.kt
@@ -1,17 +1,25 @@
 package com.nabilakhairunnisa0028.asesmen1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -23,7 +31,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -57,6 +67,13 @@ fun ScreenContent(modifier: Modifier = Modifier){
 
     var jumlah by rememberSaveable { mutableStateOf("") }
 
+    val radioOptions = listOf(
+        stringResource(R.string.tip_0),
+        stringResource(R.string.tip_5),
+        stringResource(R.string.tip_10)
+    )
+    var tip by rememberSaveable { mutableStateOf(radioOptions[0]) }
+
     Column(
         modifier = modifier.fillMaxSize()
             .verticalScroll(rememberScrollState())
@@ -67,7 +84,6 @@ fun ScreenContent(modifier: Modifier = Modifier){
         Text(
             text = stringResource(id = R.string.app_intro),
             style = MaterialTheme.typography.bodyLarge,
-            modifier =modifier.fillMaxWidth()
         )
         OutlinedTextField(
             value = total,
@@ -92,6 +108,56 @@ fun ScreenContent(modifier: Modifier = Modifier){
                 imeAction = ImeAction.Next
             ),
             modifier = Modifier.fillMaxWidth()
+        )
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = 8.dp),
+            thickness = 1.dp
+        )
+        Text(
+            text = stringResource(id = R.string.tip_title),
+            style = MaterialTheme.typography.bodyLarge,
+        )
+        Row(
+            modifier = Modifier
+                .padding(top = 6.dp)
+                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
+        ) {
+            radioOptions.forEach { text ->
+                TipOption(
+                    label = text,
+                    isSelected = tip == text,
+                    modifier = Modifier
+                        .selectable(
+                            selected = tip == text,
+                            onClick = { tip = text },
+                            role = Role.RadioButton
+                        )
+                        .weight(1f)
+                        .padding(16.dp)
+                )
+            }
+        }
+        Button(
+            onClick = {},
+            modifier = Modifier.padding(top = 8.dp),
+            contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+        ) {
+            Text(text = stringResource(R.string.hitung))
+        }
+    }
+}
+
+@Composable
+fun TipOption(label: String, isSelected: Boolean, modifier: Modifier){
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RadioButton(selected = isSelected, onClick = null)
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(start = 8.dp)
         )
     }
 }
